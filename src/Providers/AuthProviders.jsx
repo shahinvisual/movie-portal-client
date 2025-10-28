a44300873733373594981bc9ef240cfd53cae3e3
shahinvisual <shahinalam1084@gmail.com>
2025-10-28 19:27:19 +0600

    Contect api  create and central firebase authentication providers & authObserber setup

---
+++ src/Providers/AuthProviders.jsx
@@ -0,0 +1,69 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import auth from '../Firebase/firebase.config';
+import { AuthContext } from './AuthContext';
+
+const AuthProviders = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const provider = new GoogleAuthProvider();
+    // Login With Google-------------------------
+    const loginWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    };
+
+    // User New Account Register-------------------------
+    const userRegister = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    // Login With Email & Password-------------------------
+    const loginWithPassword = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+    // // Updated Profile-------------------------
+    // const userProfileUpdate = (name, photoURL) => {
+    //     return updateProfile(auth.currentUser, {
+    //         displayName: name,
+    //         photoURL: photoURL
+    //     })
+    // };
+    // //  Password Reset---------------------
+    // const PasswordReset = (email) => {
+    //     setLoading(true)
+    //     return sendPasswordResetEmail(auth, email)
+    // }
+
+    // LogOut-------------------------
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth)
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+    const AuthInfo = {
+        user,
+        loading,
+        loginWithGoogle,
+        userRegister,
+        loginWithPassword,
+        logOut
+    }
+    return (
+        <AuthContext.Provider value={AuthInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
+import AuthProviders from './Providers/AuthProviders.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProviders>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProviders>
   </StrictMode>,
 )
