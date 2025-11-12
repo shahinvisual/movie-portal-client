1eefbee226e7184f901a97c411bb8e2c05bbb26d
shahinvisual <shahinalam1084@gmail.com>
2025-11-12 11:21:55 +0600

    search fuctionality impliment by server side and front end data get by show

--- src/Pages/AllMovies/AllMovies.jsx
+++ src/Pages/AllMovies/AllMovies.jsx
@@ -6,21 +6,31 @@ import { FaArrowRight } from "react-icons/fa6";
 import { Helmet } from "react-helmet-async";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import Swal from "sweetalert2";
 
 const AllMovies = () => {
     const AxiosUser = useAxios();
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchData, setSearchData] = useState("");
     useEffect(() => {
-        AxiosUser.get('/movieInfo')
+        AxiosUser.get(`/movieInfo?search=${searchData}`)
             .then(res => {
-                console.log(res.data);
                 setMovies(res.data);
                 setLoading(false)
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                if (error) {
+                    Swal.fire({
+                        icon: "error",
+                        title: `Oops...${error}`,
+                        text: "Something went wrong!",
+                        footer: '<a href="#">Why do I have this issue?</a>'
+                    });
+                }
+            })
 
-    }, [AxiosUser])
+    }, [AxiosUser, searchData])
     if (loading) {
         return <span className="loading loading-dots loading-xl"></span>
     }
@@ -28,6 +38,14 @@ const AllMovies = () => {
         <div className="max-w-7xl mx-auto">
             <Helmet><title>Camping Retreats || All Movie</title></Helmet>
             <Navbar></Navbar>
+            <div className="max-w-md mt-8 mx-auto">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="input w-full input-ghost"
+                    value={searchData}
+                    onChange={(e) => setSearchData(e.target.value)} />
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 {
                     movies.map((movie, index) => <MovieCard
@@ -40,7 +58,7 @@ const AllMovies = () => {
                         movie={movie} />)
                 }
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
